6c446649995af8168b4f9a710aa180f2b2e3c8d2
Gokac9 <theabraham9@gmail.com>
2024-04-03 14:26:22 +0200

    initial folders

---
+++ mirko_contacts_customization/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Mirko Vincenzo Vullo",
+    "summary": "Contacts students, parents and teachers",
+    "version": "16.0.1.0.0",
+    "category": "Contacts",
+    "author": "Manuel Calero, Salvador Gonzalez, Abraham Carrasco, Xtendoo",
+    "license": "LGPL-3",
+    "application": True,
+    "depends": [
+        "res_partner",
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
